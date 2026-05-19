ae0d307ce553ace645c7783c2739495ecde32403
Harshithamanjunatha <152621125+Harshithamanjunatha@users.noreply.github.com>
2026-05-20 00:01:41 +0530

    Deployment update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MONGO_URI = "mongodb+srv://harshithamanjunath0710_db_user:<WQfxPg5YJMMEgUVj>@cluster0.hcb78g9.mongodb.net/"
+MONGO_URI = "mongodb://localhost:27017/"
 DATABASE_NAME = "darkpatternDB"
 
 DEFAULT_CSV = os.path.join(BASE_DIR, "online_shoppers_intention.csv")
