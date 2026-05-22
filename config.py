9cd1fc061fb51aef3f2d44275dcf39bb01791373
Harshithamanjunatha <152621125+Harshithamanjunatha@users.noreply.github.com>
2026-05-22 10:45:59 +0530

    changed db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MONGO_URI = "mongodb+srv://harshithamanjunath0710_db_user:<jDkUSZe4Uh5bQ6hH>@cluster0.hcb78g9.mongodb.net/"
+MONGO_URI = "mongodb+srv://harshithamanjunath0710_db_user:<nHquTEzM5FKyuhwO>@cluster0.hcb78g9.mongodb.net/"
 DATABASE_NAME = "darkpatternDB"
 
 DEFAULT_CSV = os.path.join(BASE_DIR, "online_shoppers_intention.csv")
